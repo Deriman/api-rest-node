172bda6be62bec52ef5b3aa2f203896470026f5f
Jose C. Santos <zderimanz@hotmail.com>
2024-11-08 21:04:03 +0100

    Configuramos el express-validator para validar la data que introduce el usuario. La estrategia a usar es mediante middleware

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { readItems, readItem, createItem } = require("../controllers/tracks");
 const router = express.Router();
+const validatorCreateItem = require('../validators/tracks.js')
 
 // TODO http://localhost:3001/tracks -> POST, GET, PUT Y DELETE
 
 router.get('/', readItems);
 router.get('/:id', readItem);
-router.post('/', createItem);
+router.post('/',  validatorCreateItem, createItem);
 
 module.exports = router;
